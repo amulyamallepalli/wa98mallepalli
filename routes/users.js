e215fc66236f4000193862892276bb6abe000047
amulyamallepalli <77821601+amulyamallepalli@users.noreply.github.com>
2021-03-24 17:18:37 -0500

    users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var m=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  m+=1;
+  res.send('User accesses are: '+m);
 });
 
 module.exports = router;
